ec825374a9f35beda8a1866abd1c5ab60952f726
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-06 11:22:13 -0400

    completed method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,4 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using MethodOverloading;
+
+var answer = Methods.Add(2,2, true);
+Console.WriteLine(answer);
